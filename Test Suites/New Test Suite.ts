4b88f2104d5e55a0948ea7a3d8a0357433c7a590
linh to <linh.to@katalon.com>
2023-04-07 16:26:32 +0700

    update ts

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -16,17 +16,26 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/New Test Case</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>db94882a-8b10-41b6-9ab3-ce119642ecd4</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/New Test Data</testDataId>
+      </testDataLink>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
+         <testDataLinkId>db94882a-8b10-41b6-9ab3-ce119642ecd4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>first_name</value>
          <variableId>920e85d4-db13-469d-8064-0b347020eae4</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
+         <testDataLinkId>db94882a-8b10-41b6-9ab3-ce119642ecd4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>last_name</value>
          <variableId>31171569-fe27-47aa-bb4f-8f58c977f7c1</variableId>
       </variableLink>
    </testCaseLink>
